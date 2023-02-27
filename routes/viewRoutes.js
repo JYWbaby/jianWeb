ade80a530fdc88e56641861037c99e7128bcff29
JYWbaby <35770621+JYWbaby@users.noreply.github.com>
2023-02-26 22:17:16 -0500

    fixed header icon

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -10,7 +10,11 @@ const router = express.Router();
 //   authController.isLoggedIn,
 //   viewsController.getOverview
 // );
-router.get('/', viewsController.getUnderConstruction);
+router.get(
+  '/',
+  authController.isLoggedIn,
+  viewsController.getUnderConstruction
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
